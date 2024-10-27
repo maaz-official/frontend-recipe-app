db9ca28bc32026559143e3fca14a0246edb882a9
maaz-official <muhammadmaaz07@gmail.com>
2024-10-26 19:37:50 -0700

    add new baseURL

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,2 @@
 export const BASE_URL = "https://recipe-backend-api-nine.vercel.app"
-export const RECIPE_URL = `${BASE_URL}/api/recipes`
+export const RECIPE_URL = `${BASE_URL}/api/recipes/recipes`
